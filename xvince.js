d9610f7e4b6681b0e0f0b237c0c1dece61112a10
BISC.lua <golf.le.fleur26@outlook.com>
2025-02-14 22:26:45 -0600

    Update xvince.js

--- xvince.js
+++ xvince.js
@@ -2,11 +2,28 @@
 	Welcome to the world of XVince! The comments will guide you on the functions
  	to make you more familiar with xvince.
 */
-ver = "XV1"
-xvince = {
+let ver = "XV1";
+let xvince = {
+	VOID_FUNC: {
+		CONSOLE: {
+			OUT: (args) => {
+				console.log(test_value(args[0]))
+			}
+		}
+	},
 	
+};
+/*
+	test_value function checks if a value starts with %, if so it will reference a 
+ 	variable. It also checks if a value starts with #, if so it will call an OUT_FUNC
+*/
+function test_value(value, var_reference="%", out_func_ref="#") {
+	if (value[0] == var_reference) {
+		
+	} else if (value[0] == out_func_ref) {
+		
+	} else {return value}
 }
-
 /* 
 	lexical_split function does pretty much the same as the default split function.
  	However, this one does not split spaces that are in double quotes (") or whatever
